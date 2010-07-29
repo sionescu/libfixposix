95a75bf6cf5950387a5c56a2dc35305b0ef54171
Stelian Ionescu <sionescu@cddr.org>
2010-07-29 13:47:55 +0800

    Add select() and its helpers

---
+++ src/lib/select.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/select.h>
+
+#include <libfixposix.h>
+
+int lfp_select(int nfds, fd_set *readfds, fd_set *writefds,
+               fd_set *exceptfds, const struct timespec *timeout,
+               const sigset_t *sigmask)
+{
+    return pselect(nfds, readfds, writefds, exceptfds, timeout, sigmask);
+}
+
+void lfp_fd_clr(int fd, fd_set *set)
+{
+    FD_CLR(fd, set);
+}
+
+bool lfp_fd_isset(int fd, fd_set *set)
+{
+    return (bool) FD_ISSET(fd, set);
+}
+
+void lfp_fd_set(int fd, fd_set *set)
+{
+    FD_SET(fd, set);
+}
+
+void lfp_fd_zero(fd_set *set)
+{
+    FD_ZERO(set);
+}
