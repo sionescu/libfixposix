2862f2005426d08619488d58226f712d0e8bd4e9
Stelian Ionescu <sionescu@cddr.org>
2010-07-30 22:06:33 +0800

    More fixes for lfp_pipe()

--- src/lib/pipe.c
+++ src/lib/pipe.c
@@ -12,19 +12,18 @@ int lfp_pipe (int pipefd[2], lfp_flags_t flags)
         // exists, which means that it's in the lower part of "flags"
         return pipe2(pipefd, flags & 0xFFFFFFFF);
     } else {
-        int ret = pipe(pipefd);
-        if (ret < 0) { goto error_return; }
-        if (flags & O_NONBLOCK) {
-            int ret = fcntl(pipefd[0], F_SETFD, FD_CLOEXEC);
-            if (ret < 0) { goto error_close; }
-            int ret = fcntl(pipefd[1], F_SETFD, FD_CLOEXEC);
-            if (ret < 0) { goto error_close; }
+        if (pipe(pipefd) < 0) {
+            goto error_return;
         }
-        if (flags & O_NONBLOCK) {
-            int ret = fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
-            if (ret < 0) { goto error_close; }
-            int ret = fcntl(pipefd[1], F_SETFL, O_NONBLOCK);
-            if (ret < 0) { goto error_close; }
+        if ((flags & O_CLOEXEC) &&
+            (fcntl(pipefd[0], F_SETFD, FD_CLOEXEC) < 0 ||
+             fcntl(pipefd[1], F_SETFD, FD_CLOEXEC) < 0)) {
+            goto error_close;
+        }
+        if ((flags & O_NONBLOCK) &&
+            (fcntl(pipefd[0], F_SETFL, O_NONBLOCK) < 0 ||
+             fcntl(pipefd[1], F_SETFL, O_NONBLOCK) < 0)) {
+            goto error_close;
         }
         return 0;
       error_close:
