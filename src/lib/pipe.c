b8d805eb1559d62aab2cf44972d826553977fabc
Stelian Ionescu <sionescu@cddr.org>
2010-07-30 21:51:49 +0800

    Fix lfp_pipe()

--- src/lib/pipe.c
+++ src/lib/pipe.c
@@ -11,21 +11,25 @@ int lfp_pipe (int pipefd[2], lfp_flags_t flags)
         // We assume that if pipe2() is defined then O_CLOEXEC too
         // exists, which means that it's in the lower part of "flags"
         return pipe2(pipefd, flags & 0xFFFFFFFF);
-    }
-    else {
-        int fd = pipe(pipefd);
-        if (fd < 0) { goto error_return; }
+    } else {
+        int ret = pipe(pipefd);
+        if (ret < 0) { goto error_return; }
         if (flags & O_NONBLOCK) {
             int ret = fcntl(pipefd[0], F_SETFD, FD_CLOEXEC);
+            if (ret < 0) { goto error_close; }
+            int ret = fcntl(pipefd[1], F_SETFD, FD_CLOEXEC);
             if (ret < 0) { goto error_close; }
         }
         if (flags & O_NONBLOCK) {
             int ret = fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
             if (ret < 0) { goto error_close; }
+            int ret = fcntl(pipefd[1], F_SETFL, O_NONBLOCK);
+            if (ret < 0) { goto error_close; }
         }
-        return fd;
+        return 0;
       error_close:
-        close(fd);
+        close(pipefd[0]);
+        close(pipefd[1]);
       error_return:
         return -1;
     }
