ce8c54d709b5be6c7389d5b3a8a9c9ef5dd0099e
Stelian Ionescu <sionescu@cddr.org>
2010-08-02 02:42:28 +0800

    Move CMSG_* accessors to socket.c

--- src/lib/socket.c
+++ src/lib/socket.c
@@ -79,3 +79,35 @@ int lfp_accept(int             sockfd,
         return -1;
     }
 }
+
+
+
+struct cmsghdr* lfp_cmsg_firsthdr (struct msghdr* msgh)
+{
+  return CMSG_FIRSTHDR(msgh);
+}
+
+struct cmsghdr* lfp_cmsg_nxthdr (struct msghdr* msgh, struct cmsghdr* cmsg)
+{
+  return CMSG_NXTHDR(msgh, cmsg);
+}
+
+size_t lfp_cmsg_align (size_t length)
+{
+  return CMSG_ALIGN(length);
+}
+
+size_t lfp_cmsg_space (size_t length)
+{
+  return CMSG_SPACE(length);
+}
+
+size_t lfp_cmsg_len (size_t length)
+{
+  return CMSG_LEN(length);
+}
+
+void* lfp_cmsg_data (struct cmsghdr* cmsg)
+{
+  return CMSG_DATA(cmsg);
+}
