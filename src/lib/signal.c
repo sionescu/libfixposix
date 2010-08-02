1ecda3f98d73cbedd11baa8010f6e2181be2f72e
Francois-Rene Rideau <fare@tunes.org>
2010-08-02 10:13:44 -0400

    Remove extern from signal.c, after Stelian's apparent change of mind.

--- src/lib/signal.c
+++ src/lib/signal.c
@@ -2,23 +2,23 @@
 
 #include <signal.h>
 
-extern sighandler_t lfp_sig_ign (void) {
+sighandler_t lfp_sig_ign (void) {
   return SIG_IGN;
 }
 
-extern sighandler_t lfp_sig_dfl (void) {
+sighandler_t lfp_sig_dfl (void) {
   return SIG_DFL;
 }
 
-extern sighandler_t lfp_sig_err (void) {
+sighandler_t lfp_sig_err (void) {
   return SIG_ERR;
 }
 
-extern int lfp_sigrtmin (void) {
+int lfp_sigrtmin (void) {
   return SIGRTMIN;
 }
 
-extern int lfp_sigrtmax (void) {
+int lfp_sigrtmax (void) {
   return SIGRTMAX;
 }
 
