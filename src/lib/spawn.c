1d79af6f9b67d96499d682879a696abf831aae0c
Stelian Ionescu <sionescu@cddr.org>
2010-08-09 12:36:10 +0100

    Split lfp_spawn()

--- src/lib/spawn.c
+++ src/lib/spawn.c
@@ -17,6 +17,44 @@ void child_exit(int pipe, int child_errno)
         _exit(254);
 }
 
+void handle_child(const char *path,
+                  char *const argv[],
+                  char *const envp[],
+                  const lfp_spawn_file_actions_t *file_actions,
+                  const lfp_spawnattr_t *attr,
+                  int pipes[2])
+{
+    close(pipes[0]);
+    int child_errno = lfp_apply_spawnattr(attr);
+    if (child_errno != 0)
+        child_exit(pipes[1], child_errno);;
+    execve(path, argv, envp);
+    child_exit(pipes[1], lfp_errno());
+}
+
+int handle_parent(pid_t *pid, pid_t child_pid, int pipes[2])
+{
+    close(pipes[1]);
+    int child_errno, status;
+    int noctets = read(pipes[0], &child_errno, sizeof(int));
+    int read_errno = lfp_errno();
+    close(pipes[0]);
+    switch (noctets) {
+    case -1:
+        SYSERR(read_errno);
+    case 0:
+        *pid = child_pid;
+        return 0;
+    case 4:
+        waitpid(child_pid, &status, WNOHANG);
+        SYSERR(child_errno);
+    default:
+        // This is not suppose to happen because all 4 octets
+        // of the child's errno should get here with one write
+        SYSERR(LFP_EBUG);
+    }
+}
+
 int lfp_spawn(pid_t *pid,
               const char *path,
               char *const argv[],
@@ -27,7 +65,7 @@ int lfp_spawn(pid_t *pid,
 {
     SYSCHECK(LFP_EINVAL, pid == NULL);
 
-    int ret, pipes[2];
+    int pipes[2];
 
     // Used for passing the error code from child to parent in case
     // some of the syscalls executed in the child fail
@@ -36,35 +74,13 @@ int lfp_spawn(pid_t *pid,
 
     pid_t child_pid = fork();
 
-    if (child_pid < 0) {
+    switch (child_pid) {
+    case -1:
         return -1;
-    } else if (child_pid == 0) { // child
-        close(pipes[0]);
-        int child_errno = lfp_apply_spawnattr(attr);
-        if (child_errno != 0)
-            child_exit(pipes[1], child_errno);;
-        execve(path, argv, envp);
-        child_exit(pipes[1], lfp_errno());
-    } else {                 // parent
-        int child_errno, read_errno, status;
-        close(pipes[1]);
-        ret = read(pipes[0], &child_errno, sizeof(int));
-        read_errno = lfp_errno();
-        close(pipes[0]);
-        switch (ret) {
-        case -1:
-            SYSERR(read_errno);
-        case 0:
-            *pid = child_pid;
-            return 0;
-        case 4:
-            waitpid(child_pid, &status, WNOHANG);
-            SYSERR(child_errno);
-        default:
-            // This is not suppose to happen because all 4 octets
-            // of the child's errno should get here with one write
-            SYSERR(EDOM);
-        }
+    case 0:
+        handle_child(path, argv, envp, file_actions, attr, pipes);
+        SYSERR(LFP_EBUG);
+    default:
+        return handle_parent(pid, child_pid, pipes);
     }
-    SYSERR(LFP_EBUG);
 }
