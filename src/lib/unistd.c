ac05a6341565368426846ef921662658c98a9882
Stelian Ionescu <sionescu@cddr.org>
2010-10-25 17:21:23 +0200

    Autoconf doesn't "#define HAVE_<FUNCTION> 0" if not found, so fix lfp_pipe()

--- src/lib/unistd.c
+++ src/lib/unistd.c
@@ -16,34 +16,35 @@ off_t lfp_lseek(int fd, off_t offset, int whence)
 
 int lfp_pipe (int pipefd[2], lfp_open_flags_t flags)
 {
-    if (HAVE_PIPE2) {
-        // We assume that if pipe2() is defined then O_CLOEXEC too
-        // exists, which means that it's in the lower part of "flags"
-        return pipe2(pipefd, flags & 0xFFFFFFFF);
-    } else {
-        if (pipe(pipefd) < 0) {
-            goto error_return;
-        }
-
-        if ((flags & O_CLOEXEC) &&
-            (lfp_set_fd_cloexec(pipefd[0], true) < 0 ||
-             lfp_set_fd_cloexec(pipefd[1], true) < 0)) {
-            goto error_close;
-        }
-        if ((flags & O_NONBLOCK) &&
-            (lfp_set_fd_nonblock(pipefd[0], true) < 0 ||
-             lfp_set_fd_nonblock(pipefd[1], true) < 0)) {
-            goto error_close;
-        }
-
-        return 0;
-
-      error_close:
-        close(pipefd[0]);
-        close(pipefd[1]);
-      error_return:
-        return -1;
+#if defined(HAVE_PIPE2)
+    // We assume that if pipe2() is defined then O_CLOEXEC too
+    // exists, which means that it's in the lower part of "flags"
+    return pipe2(pipefd, flags & 0xFFFFFFFF);
+#else
+    if (pipe(pipefd) < 0) {
+        goto error_return;
     }
+
+    if ((flags & O_CLOEXEC) &&
+        (lfp_set_fd_cloexec(pipefd[0], true) < 0 ||
+         lfp_set_fd_cloexec(pipefd[1], true) < 0)) {
+        goto error_close;
+    }
+    if ((flags & O_NONBLOCK) &&
+        (lfp_set_fd_nonblock(pipefd[0], true) < 0 ||
+         lfp_set_fd_nonblock(pipefd[1], true) < 0)) {
+        goto error_close;
+    }
+
+    return 0;
+
+  error_close:
+    close(pipefd[0]);
+    close(pipefd[1]);
+  error_return:
+    return -1;
+}
+#endif // HAVE_PIPE2
 }
 
 
