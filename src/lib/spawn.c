d0d4a6021c502204118409e23c2874dd50ed4203
Stelian Ionescu <sionescu@cddr.org>
2010-11-03 10:32:14 +0100

    Add "restrict" qualifier to signature of lfp_spawn()

--- src/lib/spawn.c
+++ src/lib/spawn.c
@@ -66,12 +66,12 @@ int handle_parent(pid_t *pid, pid_t child_pid, int pipes[2])
 
 static
 int _lfp_spawn(execfun *execfun,
-               pid_t *pid,
-               const char *path,
-               char *const argv[],
-               char *const envp[],
-               const lfp_spawn_file_actions_t *file_actions,
-               const lfp_spawnattr_t *attr)
+               pid_t *restrict pid,
+               const char *restrict path,
+               char *const argv[restrict],
+               char *const envp[restrict],
+               const lfp_spawn_file_actions_t *restrict file_actions,
+               const lfp_spawnattr_t *restrict attr)
 {
     int pipes[2];
 
@@ -94,24 +94,24 @@ int _lfp_spawn(execfun *execfun,
     }
 }
 
-int lfp_spawn(pid_t *pid,
-              const char *path,
-              char *const argv[],
-              char *const envp[],
-              const lfp_spawn_file_actions_t *file_actions,
-              const lfp_spawnattr_t *attr)
+int lfp_spawn(pid_t *restrict pid,
+              const char *restrict path,
+              char *const argv[restrict],
+              char *const envp[restrict],
+              const lfp_spawn_file_actions_t *restrict file_actions,
+              const lfp_spawnattr_t *restrict attr)
 {
     SYSCHECK(EINVAL, pid == NULL);
 
     return _lfp_spawn(&lfp_execve, pid, path, argv, envp, file_actions, attr);
 }
 
-int lfp_spawnp(pid_t *pid,
-               const char *file,
-               char *const argv[],
-               char *const envp[],
-               const lfp_spawn_file_actions_t *file_actions,
-               const lfp_spawnattr_t *attr)
+int lfp_spawnp(pid_t *restrict pid,
+               const char *restrict file,
+               char *const argv[restrict],
+               char *const envp[restrict],
+               const lfp_spawn_file_actions_t *restrict file_actions,
+               const lfp_spawnattr_t *restrict attr)
 {
     SYSCHECK(EINVAL, pid == NULL);
 
