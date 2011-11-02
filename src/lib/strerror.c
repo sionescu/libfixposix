53286415d93f1568791f3a0c45005efc838ab191
Stelian Ionescu <sionescu@cddr.org>
2011-11-02 13:53:09 +0100

    Bring back __xpg_strerror_r

--- src/lib/strerror.c
+++ src/lib/strerror.c
@@ -22,20 +22,22 @@
 /* DEALINGS IN THE SOFTWARE.                                                   */
 /*******************************************************************************/
 
-#include <config.h>
-
-// _GNU_SOURCE exposes a non-POSIX version of strerror_r
-// that's why we put it into its own file
-#undef _GNU_SOURCE
 
 #include <lfp/strerror.h>
 
 #include <string.h>
 
 #include "utils.h"
+#if defined(HAVE___XPG_STRERROR_R)
+int __xpg_strerror_r (int errnum, char *buf, size_t buflen);
+#endif
 
 int lfp_strerror(int errnum, char *buf, size_t buflen)
 {
     SYSCHECK(EINVAL, buf == NULL);
+#if defined(HAVE___XPG_STRERROR_R)
+    return __xpg_strerror_r(errnum, buf, buflen);
+#else
     return strerror_r(errnum, buf, buflen);
+#endif // HAVE___XPG_STRERROR_R
 }
