39767bf4564391a09aaa5f7088229eb73622352f
Stelian Ionescu <sionescu@cddr.org>
2022-11-12 21:27:07 -0500

    Enable more compile warnings on CI

--- config/travis.cflags.sh
+++ config/travis.cflags.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-WERR_CFLAGS="\
+WARN_CFLAGS="\
+  -O2 \
+  -Wall -Wextra \
+  -fno-common \
+  -Wshadow \
+  -Wpointer-arith \
+  -Wformat=2 \
+  -Wswitch-default \
+  -Wswitch-enum \
+  -Wcast-align \
+  -Wcast-qual \
+  -Wconversion \
+  -Wpointer-arith \
+  -Wbad-function-cast \
+  -Wstrict-overflow=5 \
+  -Winline \
+  -Wundef \
+  -Wnested-externs \
+  -Wunreachable-code \
+  -Wfloat-equal \
+  -Wstrict-aliasing=2 \
+  -Wredundant-decls \
+  -Wold-style-definition \
   -Werror=implicit-function-declaration \
+  -Werror=strict-prototypes \
+  -Werror=missing-prototypes \
+  -Werror=missing-braces \
+  -Werror=missing-field-initializers \
   -Werror=varargs \
 "
-WERR_CFLAGS_OSX="\
-  -Werror=unguarded-availability-new \
-"
 
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-    WERR_CFLAGS+=" ${WERR_CFLAGS_OSX}";
+if [[ ${TRAVIS_OS_NAME} = "osx" ]]; then
+    WARN_CFLAGS+=" -Werror=unguarded-availability-new";
 fi
 
-export WERR_CFLAGS
+if [[ ${CC_FOR_BUILD} == "gcc" ]]; then
+    WARN_CFLAGS+=" -Wlogical-op";
+fi
+
+export WARN_CFLAGS
